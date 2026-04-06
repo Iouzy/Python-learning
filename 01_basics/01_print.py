cba99ab3b94cfcea7d4c9f00bd4c2084f0732d70
iouzy <88393641+Iouzy@users.noreply.github.com>
2026-04-07 00:25:20 +0100

    01_print.py basics

--- 01_basics/01_print.py
+++ 01_basics/01_print.py
@@ -1,1 +1,46 @@
-print("Hello world.")
+# =====================
+# BASIC PRINT EXAMPLES
+# =====================
+
+# Basic print
+print("Hello, world")
+
+# Multiple values
+print("Hello", "Richard")
+
+# Numbers
+print(10)
+print(10 + 5)
+
+# Variables
+name = "Richard"
+age = 25
+
+print("My name is", name)
+print("I am", age, "years old")
+
+# f-strings (importante)
+print(f"My name is {name} and I am {age} years old")
+
+# Escape characters
+print("Line1\nLine2")
+
+# Separator
+print("A", "B", "C", sep="-")
+
+# End parameter
+print("This is the end", end="!")
+print(" Next line")
+
+# =====================
+# EXERCISES
+# =====================
+
+city = "Lisbon"
+
+print(f"My name is {name}, I live in {city}, and I am {age} years old.")
+
+print("A", "B", sep="---", end="###")
+print("C")
+
+
