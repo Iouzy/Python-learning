9d5d6e3b2c22aefc31531c5038cd8a5bfc47aafc
iouzy <88393641+Iouzy@users.noreply.github.com>
2026-04-07 00:25:41 +0100

    02_variables.py basics

---
+++ 01_basics/02_variables.py
@@ -0,0 +1,30 @@
+# Variables
+name = "Richard"
+age = 25
+height = 1.75
+is_student = False
+
+print(name, type(name))
+print(age, type(age))
+print(height, type(height))
+print(is_student, type(is_student))
+
+# Changing variables
+age = 30
+print(age)
+
+# Multiple assignment
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+# Exercise
+
+name = "Richard"
+birth_year = 2000
+
+current_year = 2026
+age = current_year - birth_year
+
+print(f"{name} is {age} years old.")
+
+
